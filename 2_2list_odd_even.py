57af2bbbcd80ed2c9f8036aa9e57eb180e44c940
collage pc <your_email@example.com>
2024-03-18 08:09:49 +0530

    update 2.2

--- 2_2list_odd_even.py
+++ 2_2list_odd_even.py
@@ -1,28 +1,25 @@
 #integers=[]
 odd=[]
 even=[]
-e,o=0,0
 size=int(input("Enter the size of the list:: "))
 
 for i in range(size):
     element=int(input(f"Enter the {i+1} integer: "))
     if element%2 == 0:
         even.append(element)
-        e=e+1
 
     else :
         odd.append(element)
-        o=o+1
 
     #integers.append(element)
 
 
 print("The EVEN Elements of the List are:: ")
-for i in range(e):
-    print(even[i],end=", ")
+for i in even:
+    print(i,end=", ")
 print()
 
 print("The ODD Elements of the List are:: ")
-for i in range(o):
-    print(odd[i],end=", ")
+for i in odd:
+    print(i,end=", ")
 

d752276524671d9c4de265b0b2fceb3a743c6783
Deb kumar Mondal <visitdebkumarkatwa@gmail.com>
2024-03-24 01:21:13 +0530

    update files

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 lst=[]
+size=0
 
 while(True) :
     print("****************MENU****************")
@@ -10,14 +11,12 @@ while(True) :
     print("6=Exit")
     print("====================================")
 
-    choice=int(input("What Are you want to Do:: "))
+    choice=input("What Are you want to Do:: ")
     
-    if choice==1 :
-        for i in lst :
-            print(i,end=", ")
-        print()
+    if choice=='1' :
+        print(lst)
     
-    elif choice==2 :
+    elif choice=='2' :
         find=input("Enter the element you want to find: ")
 
         for i in range(size):
@@ -26,21 +25,38 @@ while(True) :
                 break
         else :
             print("SORRY!! The element is not in the list........")
+            while True:
+                print("Are you want to add the element in the list?????\nif you want to add press 1 else press 0::")
+                ans=int(input())
+                if ans ==1:
+                    lst.append(find)
+                    print("Element added successfully!!!\n\n")
+                    break
+                elif ans==0:
+                    print("Okay!Thanks for using.....")
+                    break
+                else:
+                    print("Invalid input......")
+                    print("press a correct input\n\n")
 
-    elif choice==3 :
+    elif choice=='3' :
         element=input("Enter the new element: ")
 
-        lst.extend(element)
+        lst.append(element)
+        size+=1
+        print("Insertion Completed")
         
-    elif choice==4 :
+    elif choice=='4' :
         index=int(input("Which Index's element are you want to delete:: "))
         
         lst.pop(index)
+        print("Deletion Completed")
+
  
-    elif choice==5 :
+    elif choice=='5' :
         print(f"The lenth of the list is {len(lst)}")
         
-    elif choice==6 :
+    elif choice=='6' :
         break
     
     else :

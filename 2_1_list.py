1062498c749ebf913ad68640117df59637b0f250
Debkumarkatwa <159232491+Debkumarkatwa@users.noreply.github.com>
2024-03-28 10:43:58 +0530

    Update 2_1_list.py

--- 2_1_list.py
+++ 2_1_list.py
@@ -1,9 +1,10 @@
 lst=[]
-
+size=0
 while  True:
     element=input(f"Enter the {size+1} element: ")
     if element!= 'q':
         lst.append((element))
+        size+=1
     else:
         print("The Elements of the List are:: ")
         print(lst)
